3bd6056a9b42c2209335ea8c5347a8d53288eb9b
victor kirilov <n05moke@outlook.com>
2019-03-20 12:13:43 +0300

    Зафиксировать всё

--- BasikProject/BasikProject/urls.py
+++ BasikProject/BasikProject/urls.py
@@ -3,19 +3,10 @@ Definition of urls for BasikProject.
 """
 
 from django.conf.urls import include, url
+import HelloDjangoApp.views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
+# Django processes URL patterns in the order they appear in the array
 urlpatterns = [
-    # Examples:
-    # url(r'^$', BasikProject.views.home, name='home'),
-    # url(r'^BasikProject/', include('BasikProject.BasikProject.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]
