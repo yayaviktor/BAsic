c540491d1332a6a2acf8d9225e8624b9b01e72c1
victor kirilov <n05moke@outlook.com>
2019-03-20 13:03:16 +0300

    зафиксировать всё

--- BasikProject/BasikProject/urls.py
+++ BasikProject/BasikProject/urls.py
@@ -9,4 +9,5 @@ import HelloDjangoApp.views
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
 ]
